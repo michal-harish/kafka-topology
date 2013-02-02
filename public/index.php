d4017a380e1a3d4165d6f948061fdb9f2e0813b1
Michal Harish <michal.harish@gmail.com>
2013-02-02 00:58:28 +0000

    Added gridportal and simple portlet for information about individual cluster.

---
+++ public/index.php
@@ -0,0 +1,44 @@
+<?php 
+
+require '../gridportal/src/.php';
+$gridportal_debug_mode = true;
+
+?><!DOCTYPE html>
+<html xmlns:g="portal/portlets.xsd">
+<head>
+	<!-- standard html head -->
+	<title>Kafka PHP Monitor</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+	<!-- 
+	<link rel="stylesheet" type="text/css" href="http://media.gridport.co/aos/main.css" />
+	 -->
+	<g:portlet id="local" url="/kafka-topology/portlet_cluster_info.php?localhost:2181"/>
+	<g:portlet id="aos3" url="/kafka-topology/portlet_cluster_info.php?aos3.gridport.co:2181"/>
+</head>
+<body>
+	<h2><span>Kafka Topoogy Scanner</span></h2>
+	<table border="1">
+		<tr>
+			<th>local kafka brokers / topics</th>
+		</tr>
+		<tr>
+			<td>										
+				<g:portlet id="local" fragment="brokers"/>
+				<hr/>
+				<g:portlet id="local" fragment="topics"/>
+			</td>
+		</tr>
+		<tr><td style="background:silver;"></td></tr>	
+		<tr>
+			<th>aos3 kafka brokers & topics</th>
+		</tr>		
+		<tr>
+			<td>
+				<g:portlet id="aos3" fragment="brokers"/>
+				<hr/>
+				<g:portlet id="aos3" fragment="topics"/>
+			</td>
+		</tr>
+	</table>
+</body>
+</html>
---
+++ public/portlet_cluster_info.php
@@ -0,0 +1,77 @@
+<?php
+	require __DIR__ . '/../kafka-php/src/Kafka/Kafka.php';
+	header("Cache-Control: max-age=10, no-store"); //must-revalidate
+	
+	$connectionString = urldecode($_SERVER['QUERY_STRING']);
+	$zk = new ZooKeeper($connectionString);
+	
+	$brokers = array();
+	if ($brokerIds = @$zk->getChildren('/brokers/ids'))
+	{
+		foreach($brokerIds as $brokerId)
+		{
+			$brokerHash = @$zk->get("/brokers/ids/{$brokerId}");
+			$hostPort = explode(":",$brokerHash);
+			$brokers[$brokerId] = "{$hostPort[1]}:{$hostPort[2]}";
+			$kafka = new \Kafka\Kafka($hostPort[1], $hostPort[2], 30);
+			$consumers[$brokerId] = $kafka->createConsumer(); 
+		}
+	}
+	
+	$topics = array();
+	if ($topicNames = @$zk->getChildren('/brokers/topics'))
+	{
+		foreach($topicNames as $topicName)
+		{
+			foreach($brokers as $brokerId => $brokerName)
+			{		
+				$brokerPartitionCount = @$zk->get("/brokers/topics/{$topicName}/{$brokerId}");
+				for($partition=0; $partition<$brokerPartitionCount; $partition++)
+				{
+					$topics[$topicName]["{$brokerId}-{$partition}"] = array(
+						'earliest' => array_shift($consumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_EARLIEST)),
+					    'latest' => array_shift($consumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_LATEST)),
+					);
+				}
+			}
+		}
+	}
+	
+	$colors[0] = 'blue';
+	$colors[1] = 'green';
+	$colors[2] = 'maroon';
+	
+?>
+<body>
+	<div id='brokers'>		
+		<?php if ($brokers !== null) :?>
+		<ul>
+		<?php foreach($brokers as $brokerId => $brokerName) : ?>
+			<li><?php echo "[{$brokerId}] ";?><small><?php echo $brokerName;?></small></li>		
+		<?php endforeach;?>
+		</ul>
+	</div>
+	<div id='topics'>		
+		<ul>			
+			<?php foreach($topics as $topicName => $partitions) : ?>
+			<li>		
+				<b><?php echo $topicName;?></b>		
+				<?php $p=0;foreach($partitions as $partition => $offsets) : ?>
+				<span style="color: <?php echo $colors[$p++];?>;"><?php echo " [<b>{$partition}</b>] ";?>
+				<small><?php echo trim($offsets['earliest'],'0') .'-';?></small>
+				<small><?php echo trim($offsets['latest'],0);?></small>
+				</span>
+				<?php endforeach;?>
+			</li>
+			<?php endforeach;?>
+		</ul>		
+
+		<?php endif;?>
+	</div>
+	
+	<pre id ='inspector'><?php
+	//print_r($zk->getChildren('/brokers/topics'));
+	
+	?></pre>
+	
+</body>
