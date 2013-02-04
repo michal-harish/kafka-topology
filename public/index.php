3abda988c1474b928ab4560d644995449ff9e364
Michal Haris <michal.haris@visualdna.com>
2013-02-04 18:45:19 +0000

    Cleanup

--- public/index.php
+++ public/index.php
@@ -14,17 +14,24 @@ if (!$_SERVER['QUERY_STRING']) {
 	<?php foreach($_GET as $key => $zkConnection) : ?>
 		<g:portlet 
 			id="<?php echo $zkConnection?>" 
-			url="<?php echo $uriBase;?>/portlet_cluster.php?<?php echo $zkConnection?>"
+			url="<?php echo $uriBase;?>/portlet_cluster.phtml?<?php echo $zkConnection?>"
 		/>
+		<g:portlet id="test" url="<?php echo $uriBase;?>/portlet_test.phtml"/>
 	<?php endforeach ?>
 </head>
 <body>
 	<h2><span>Kafka Topology Scanner</span></h2>
+	<!--
+	<g:portlet id="test" class="ajax" fragment="time"/>
+	<g:portlet id="test" fragment="date"/>
+	--> 
 	<hr/>	
 	<?php foreach($_GET as $key => $zkConnection) : ?>	
 	<h3>'<?php echo $key?>' kafka cluster</h3>
+
 	<g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="topicsTable"/>
 	<g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="consumerList"/>
-	<?php endforeach;?>		
+
+	<?php endforeach;?>
 </body>
 </html>
