c35a0d497e2d04b9a19d019a7e44bba663b248e0
Michal Haris <michal.haris@visualdna.com>
2013-02-05 12:28:38 +0000

    Active and Inactive groups in separate lists.

--- public/index.php
+++ public/index.php
@@ -11,20 +11,20 @@ if (!$_SERVER['QUERY_STRING']) {
 	<title>Kafka PHP Monitor</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 	<link rel="stylesheet" type="text/css" href="<?php echo $uriBase;?>/main.css" />
+	<g:portlet id="test" url="<?php echo $uriBase;?>/portlet_test.phtml"/>
 	<?php foreach($_GET as $key => $zkConnection) : ?>
 		<g:portlet 
 			id="<?php echo $zkConnection?>" 
 			url="<?php echo $uriBase;?>/portlet_cluster.phtml?<?php echo $zkConnection?>"
 		/>
-		<g:portlet id="test" url="<?php echo $uriBase;?>/portlet_test.phtml"/>
 	<?php endforeach ?>
 </head>
 <body>
 	<h2><span>Kafka Topology Scanner</span></h2>
-	<!--
+	
 	<g:portlet id="test" class="ajax" fragment="time"/>
 	<g:portlet id="test" fragment="date"/>
-	--> 
+	 
 	<hr/>	
 	<?php foreach($_GET as $key => $zkConnection) : ?>	
 	<h3>'<?php echo $key?>' kafka cluster</h3>
