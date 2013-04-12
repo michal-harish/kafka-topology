f398b6d8025a3a0a386670ce5e009bd4c4404284
Michal Harish <michal.haris@imagini.net>
2013-04-12 18:19:46 +0100

    URI query string param for individual consumer group

--- public/index.php
+++ public/index.php
@@ -2,37 +2,37 @@
 require '../gridportal/src/.php';
 $uriBase = array_shift(preg_split('/\/*\?/', $_SERVER['REQUEST_URI']));
 if (!$_SERVER['QUERY_STRING']) {
-	exit(header("Location: ?Local=localhost:2181"));
+    exit(header("Location: ?Local=localhost:2181"));
 }
 ?><!DOCTYPE html>
 <html xmlns:g="portal/portlets.xsd">
 <head>
-	<!-- standard html head -->
-	<title>Kafka Scanner</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
-	<link rel="stylesheet" type="text/css" href="<?php echo $uriBase;?>/main.css" />
-	<g:portlet id="test" url="<?php echo $uriBase;?>/portlet_test.php"/>
-	<?php foreach($_GET as $key => $zkConnection) : ?>
-		<g:portlet 
-			id="<?php echo $zkConnection?>" 
-			url="<?php echo $uriBase;?>/portlet_cluster.php?<?php echo $zkConnection?>"
-		/>
-	<?php endforeach ?>
+    <!-- standard html head -->
+    <title>Kafka Scanner</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="<?php echo $uriBase;?>/main.css" />
+    <g:portlet id="test" url="<?php echo $uriBase;?>/portlet_test.php"/>
+    <?php foreach($_GET as $key => $zkConnection) : ?>
+        <g:portlet 
+            id="<?php echo $zkConnection?>" 
+            url="<?php echo $uriBase;?>/portlet_cluster.php?<?php echo $zkConnection?>"
+        />
+    <?php endforeach ?>
 </head>
 <body>
-	<h2><span>Kafka Topology Scanner</span></h2>
-	
-	<g:portlet id="test" class="ajax" fragment="time"/>
-	<g:portlet id="test" fragment="date"/>
-	 
-	<hr/>	
-	<?php foreach($_GET as $key => $zkConnection) : ?>
-	   <div class="clusterInstance">
-	   <h3>'<?php echo $key?>' kafka cluster</h3>
+    <h2><span>Kafka Topology Scanner</span></h2>
+    
+    <g:portlet id="test" class="ajax" fragment="time"/>
+    <g:portlet id="test" fragment="date"/>
+     
+    <hr/>    
+    <?php foreach($_GET as $key => $zkConnection) : ?>
+       <div class="clusterInstance">
+       <h3>'<?php echo $key?>' kafka cluster</h3>
 
-	   <g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="topicsTable"/>
-	   <g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="consumerList"/>
+       <g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="topicsTable"/>
+       <g:portlet class="ajax" id="<?php echo $zkConnection?>" fragment="consumerList"/>
        </div>
-	<?php endforeach;?>
+    <?php endforeach;?>
 </body>
 </html>
--- public/portlet_cluster.php
+++ public/portlet_cluster.php
@@ -1,202 +1,212 @@
 <?php
-	require __DIR__ . '/../kafka-php/src/Kafka/Kafka.php';
+    require __DIR__ . '/../kafka-php/src/Kafka/Kafka.php';
 
-	header("Cache-Control: max-age=0, no-store");
+    header("Cache-Control: max-age=0, no-store");
 
-	ini_set("zookeeper.recv_timeout", "30000");
-	$connectionString = urldecode($_SERVER['QUERY_STRING']);
-	$zk = new ZooKeeper($connectionString);
+    ini_set("zookeeper.recv_timeout", "30000");
+    $query = preg_split("/\\;/", urldecode($_SERVER['QUERY_STRING']));
+    $connectionString = $query[0];
+    if (isset($query[1])) {
+        $consumerGroup = $query[1];
+    }
+    $zk = new ZooKeeper($connectionString);
 
-	$consumers =   @$zk->exists('/consumers') ? array_fill_keys(@$zk->getChildren('/consumers'), array()) : array();
-	class watcher {
-		private $changedPath = null;
-		public function __construct($zk, array $paths, $min_sleep_sec = 1) {
-		    foreach($paths as $path) {
-		        if (@$zk->exists($path)) @$zk->getChildren($path, array($this, 'watch'));
-		    }
-		    $t = microtime(true);
-		    while (microtime(true) - $t < 60 * 5 && $this->changedPath === null) sleep($min_sleep_sec);
-		}
-		public function watch($event, $key, $path) {
-			$this->changedPath = $path;
-		}
-	}
-	if (isset($_SERVER['HTTP_PORTLET_AJAX'])) 
-	{
-		$watchPaths = array('/brokers/ids', '/consumers');
-		foreach(array_keys($consumers) as $consumer) {
-			$watchPaths[] = "/consumers/{$consumer}/ids";
-		}
-		new watcher($zk, $watchPaths);
-		sleep(3);
-	}
+    if (isset($consumerGroup)) {
+        $consumers =   @$zk->exists("/consumers/$consumerGroup") ? array($consumerGroup=>array()) : array();
+        $topicNames = @$zk->getChildren("/consumers/$consumerGroup/offsets"); 
+    } else {
+        $consumers =   @$zk->exists('/consumers') ? array_fill_keys(@$zk->getChildren('/consumers'), array()) : array();
+        $topicNames = $topicNames = @$zk->getChildren('/brokers/topics');
+        class watcher {
+            private $changedPath = null;
+            public function __construct($zk, array $paths, $min_sleep_sec = 1) {
+                foreach($paths as $path) {
+                    if (@$zk->exists($path)) @$zk->getChildren($path, array($this, 'watch'));
+                }
+                $t = microtime(true);
+                while (microtime(true) - $t < 60 * 5 && $this->changedPath === null) sleep($min_sleep_sec);
+            }
+            public function watch($event, $key, $path) {
+                $this->changedPath = $path;
+            }
+        }
+    }
+    if (isset($_SERVER['HTTP_PORTLET_AJAX'])) 
+    {
+        $watchPaths = array('/brokers/ids', '/consumers');
+        foreach(array_keys($consumers) as $consumer) {
+            $watchPaths[] = "/consumers/{$consumer}/ids";
+        }
+        new watcher($zk, $watchPaths);
+        sleep(3);
+    }
 
-	$brokers = array();
-	if ($brokerIds = $zk->getChildren('/brokers/ids'))
-	{
-		foreach($brokerIds as $brokerId)
-		{
-			$brokerHash = @$zk->get("/brokers/ids/{$brokerId}");
-			$hostPort = explode(":",$brokerHash);
-			$brokers[$brokerId] = "{$hostPort[1]}:{$hostPort[2]}";
-			$kafka = new \Kafka\Kafka($hostPort[1], $hostPort[2], 30);
-			$kafkaConsumers[$brokerId] = $kafka->createConsumer(); 
-		}
-	}
-	$topics = array();
+    $brokers = array();
+    if ($brokerIds = $zk->getChildren('/brokers/ids'))
+    {
+        foreach($brokerIds as $brokerId)
+        {
+            $brokerHash = @$zk->get("/brokers/ids/{$brokerId}");
+            $hostPort = explode(":",$brokerHash);
+            $brokers[$brokerId] = "{$hostPort[1]}:{$hostPort[2]}";
+            $kafka = new \Kafka\Kafka($hostPort[1], $hostPort[2], 30);
+            $kafkaConsumers[$brokerId] = $kafka->createConsumer(); 
+        }
+    }
+    $topics = array();
 
 try {
-	if ($topicNames = @$zk->getChildren('/brokers/topics'))
-	{
-		sort($topicNames);
-		foreach($topicNames as $topicName)
-		{
-			foreach($brokers as $brokerId => $brokerName)
-			{
-				$brokerPartitionCount = @$zk->get("/brokers/topics/{$topicName}/{$brokerId}");
-				for($partition=0; $partition<$brokerPartitionCount; $partition++)
-				{
-					try {
-						$partitionId = "{$brokerId}-{$partition}";
-						$smallest = array_shift($kafkaConsumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_EARLIEST));
-						$largest = array_shift($kafkaConsumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_LATEST));
-						$partitionStatus = array(
-							'id' => $partitionId,
-							'smallest' => $smallest->__toString(),
-						    'largest' => $largest->__toString(),
-						);
-						$topics[$topicName][$partitionId] = $partitionStatus;
-						$sections[$topicName][$brokerId][$partition] = $partitionStatus;
-					} catch (\Kafka\Exception $e) {
-						continue;
-					}
-				}
-			}
-		}
-	}
+    if ($topicNames)
+    {
+        sort($topicNames);
+        foreach($topicNames as $topicName)
+        {
+            foreach($brokers as $brokerId => $brokerName)
+            {
+                $brokerPartitionCount = @$zk->get("/brokers/topics/{$topicName}/{$brokerId}");
+                for($partition=0; $partition<$brokerPartitionCount; $partition++)
+                {
+                    try {
+                        $partitionId = "{$brokerId}-{$partition}";
+                        $smallest = array_shift($kafkaConsumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_EARLIEST));
+                        $largest = array_shift($kafkaConsumers[$brokerId]->offsets($topicName,$partition, \Kafka\Kafka::OFFSETS_LATEST));
+                        $partitionStatus = array(
+                            'id' => $partitionId,
+                            'smallest' => $smallest->__toString(),
+                            'largest' => $largest->__toString(),
+                        );
+                        $topics[$topicName][$partitionId] = $partitionStatus;
+                        $sections[$topicName][$brokerId][$partition] = $partitionStatus;
+                    } catch (\Kafka\Exception $e) {
+                        continue;
+                    }
+                }
+            }
+        }
+    }
 } catch (Exception $e) {
     echo get_class($e) . $e->getMessage();
 }
     $hasAbandonedConsumers = false;
     $hasActiveConsumers = false;
     $hasInactiveConsumers = false;
-	foreach(array_keys($consumers) as $consumerId)
-	{
-		$consumerInfo = array(
-		    'id' => $consumerId,
-		    'active' => false,
-		    'abandoned' => true,
-		    'process' => array(),
-		);
-		if ($consumerIds = @$zk->getChildren("/consumers/{$consumerId}/ids")) {
-    		foreach($consumerIds as $process) {
-    		    $consumerInfo['process'][$process] = array();
-    		}
-		}
+    foreach(array_keys($consumers) as $consumerId)
+    {
+        $consumerInfo = array(
+            'id' => $consumerId,
+            'active' => false,
+            'abandoned' => true,
+            'process' => array(),
+        );
+        if ($consumerIds = @$zk->getChildren("/consumers/{$consumerId}/ids")) {
+            foreach($consumerIds as $process) {
+                $consumerInfo['process'][$process] = array();
+            }
+        }
 
-		foreach(@$zk->getChildren("/consumers/{$consumerId}/offsets") as $topicName) {
-			if (!isset($topics[$topicName])) continue;
-			$consumerTopic = array(
-			    'active' => false,
-			    'abandoned' => true,
-			    'lagging' => false,
-			    'process' => array(),
-			    'partition' => array(),
-			);
-			foreach(@$zk->getChildren("/consumers/{$consumerId}/offsets/{$topicName}") as $partitionId) {
-				$consumerPartitionStatus['watermark'] = @$zk->get("/consumers/{$consumerId}/offsets/{$topicName}/$partitionId");
-				$smallest = $topics[$topicName][$partitionId]['smallest'];
-				$largest = $topics[$topicName][$partitionId]['largest'];
-				if (intval($largest) - intval($smallest) == 0) {
-				    $consumerPartitionStatus['progress'] = $consumerPartitionStatus['watermark'] == $largest ? 100 : false;
-				} elseif ($consumerPartitionStatus['watermark'] < $smallest) {
-				    $consumerPartitionStatus['progress'] = false;
-				} elseif (is_numeric($smallest) && is_numeric($largest)) {
-    				$consumerPartitionStatus['progress'] = round(
-    				    100 * (intval($consumerPartitionStatus['watermark']) - intval($smallest))
-    				    / (intval($largest) - intval($smallest))
-    				    ,2
-				    );
-    				$consumerTopic['lagging'] = $consumerTopic['lagging'] || ($consumerPartitionStatus['progress'] < 90);
-				}
-				$consumerTopic['partition'][$partitionId] = $consumerPartitionStatus;
-				$consumerTopic['abandoned'] = $consumerTopic['abandoned'] && $consumerPartitionStatus['progress'] === false;
-			}
+        foreach(@$zk->getChildren("/consumers/{$consumerId}/offsets") as $topicName) {
+            if (!isset($topics[$topicName])) continue;
+            $consumerTopic = array(
+                'active' => false,
+                'abandoned' => true,
+                'lagging' => false,
+                'process' => array(),
+                'partition' => array(),
+            );
+            foreach(@$zk->getChildren("/consumers/{$consumerId}/offsets/{$topicName}") as $partitionId) {
+                $consumerPartitionStatus['watermark'] = @$zk->get("/consumers/{$consumerId}/offsets/{$topicName}/$partitionId");
+                $smallest = $topics[$topicName][$partitionId]['smallest'];
+                $largest = $topics[$topicName][$partitionId]['largest'];
+                if (intval($largest) - intval($smallest) == 0) {
+                    $consumerPartitionStatus['progress'] = $consumerPartitionStatus['watermark'] == $largest ? 100 : false;
+                } elseif ($consumerPartitionStatus['watermark'] < $smallest) {
+                    $consumerPartitionStatus['progress'] = false;
+                } elseif (is_numeric($smallest) && is_numeric($largest)) {
+                    $consumerPartitionStatus['progress'] = round(
+                        100 * (intval($consumerPartitionStatus['watermark']) - intval($smallest))
+                        / (intval($largest) - intval($smallest))
+                        ,2
+                    );
+                    $consumerTopic['lagging'] = $consumerTopic['lagging'] || ($consumerPartitionStatus['progress'] < 90);
+                }
+                $consumerTopic['partition'][$partitionId] = $consumerPartitionStatus;
+                $consumerTopic['abandoned'] = $consumerTopic['abandoned'] && $consumerPartitionStatus['progress'] === false;
+            }
 
-			try {
-			  $consumerTopicOwners = @$zk->getChildren("/consumers/{$consumerId}/owners/{$topicName}");
-			  if ($consumerTopicOwners) {
-				foreach($consumerTopicOwners as $consumerProcess) {
-					$consumerTopic['process'][$consumerProcess] = array();
-				}
-		      }
-			} catch (Exception $e) { echo $e->getMessage(); }
-			$consumerTopic['active'] = isset($consumerTopic['process']) && count($consumerTopic['process']) > 0;
-			$consumerInfo['topics'][$topicName] = $consumerTopic;
-			$consumerInfo['abandoned'] &= $consumerTopic['abandoned'] && !$consumerTopic['active'];
-			$hasAbandonedConsumers |= $consumerInfo['abandoned']; 
-			$hasInactiveConsumers |= !$consumerTopic['active'] && !$consumerInfo['abandoned'];
-		}
-		$consumerInfo['active'] = count($consumerInfo['process']) > 0;
-		$hasActiveConsumers |= $consumerInfo['active'];
-		$consumers[$consumerId] = $consumerInfo;
-	}
+            try {
+              $consumerTopicOwners = @$zk->getChildren("/consumers/{$consumerId}/owners/{$topicName}");
+              if ($consumerTopicOwners) {
+                foreach($consumerTopicOwners as $consumerProcess) {
+                    $consumerTopic['process'][$consumerProcess] = array();
+                }
+              }
+            } catch (Exception $e) { echo $e->getMessage(); }
+            $consumerTopic['active'] = isset($consumerTopic['process']) && count($consumerTopic['process']) > 0;
+            $consumerInfo['topics'][$topicName] = $consumerTopic;
+            $consumerInfo['abandoned'] &= $consumerTopic['abandoned'] && !$consumerTopic['active'];
+            $hasAbandonedConsumers |= $consumerInfo['abandoned']; 
+            $hasInactiveConsumers |= !$consumerTopic['active'] && !$consumerInfo['abandoned'];
+        }
+        $consumerInfo['active'] = count($consumerInfo['process']) > 0;
+        $hasActiveConsumers |= $consumerInfo['active'];
+        $consumers[$consumerId] = $consumerInfo;
+    }
 
-	unset($zk);
+    unset($zk);
 ?>
 <body>
-	<div id='topicsTable'>
-		<?php if ($brokers !== null) :?>
-		<table>
-			<thead>
-				<tr>
-					<th><?php echo $connectionString;?></th>
-				<?php foreach($topics as $topicName => $partitions) : ?>
-					<th><?php echo "$topicName(".count($partitions).")"; ?></th>
-				<?php endforeach;?>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach($brokers as $brokerId => $brokerName) : ?>
-					<tr>
-						<th><?php echo "[{$brokerId}] ";?><small><?php echo $brokerName;?></small></th>
-						<?php foreach(array_keys($topics) as $topicName): ?>
-						<td>
-							<?php $p=0;if (isset($sections[$topicName][$brokerId])) 
-							foreach($sections[$topicName][$brokerId] as $partition => $status) : ?>
-							<small class="alterColor<?php echo ++$p;?>"><?php 
-								$id = str_replace("-", "&nbsp;-&nbsp;", $status['id']);
-								echo " [&nbsp;<b>{$id}</b>&nbsp;]&nbsp;"
-									.trim($status['smallest'],'0') .'&nbsp;-&nbsp;'
-									.trim($status['largest'],0);?>
-							</small><br/>
-							<?php endforeach;?>
-						</td>
-						<?php endforeach; ?>
-					</tr>		
-				<?php endforeach;?>
-			</tbody>
-		</table>
-		<?php endif;?>
-	</div>
+    <div id='topicsTable'>
+        <?php if ($brokers !== null) :?>
+        <table>
+            <thead>
+                <tr>
+                    <th><?php echo $connectionString;?></th>
+                <?php foreach($topics as $topicName => $partitions) : ?>
+                    <th><?php echo "$topicName(".count($partitions).")"; ?></th>
+                <?php endforeach;?>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($brokers as $brokerId => $brokerName) : ?>
+                    <tr>
+                        <th><?php echo "[{$brokerId}] ";?><small><?php echo $brokerName;?></small></th>
+                        <?php foreach(array_keys($topics) as $topicName): ?>
+                        <td>
+                            <?php $p=0;if (isset($sections[$topicName][$brokerId])) 
+                            foreach($sections[$topicName][$brokerId] as $partition => $status) : ?>
+                            <small class="alterColor<?php echo ++$p;?>"><?php 
+                                $id = str_replace("-", "&nbsp;-&nbsp;", $status['id']);
+                                echo " [&nbsp;<b>{$id}</b>&nbsp;]&nbsp;"
+                                    .trim($status['smallest'],'0') .'&nbsp;-&nbsp;'
+                                    .trim($status['largest'],0);?>
+                            </small><br/>
+                            <?php endforeach;?>
+                        </td>
+                        <?php endforeach; ?>
+                    </tr>        
+                <?php endforeach;?>
+            </tbody>
+        </table>
+        <?php endif;?>
+    </div>
 
-	<table id='consumerList'>	
-	    <tr>
+    <table id='consumerList'>    
+        <tr>
         <?php if ($consumers !== null) :?>
-	    <td>
-	       <?php if ($hasActiveConsumers) : ?>
-	       <h3 class="active" >Active consumer groups</h3>
-	       <ul>
-			<?php foreach($consumers as $consumerId => $consumerInfo) if ($consumerInfo['active']) : ?>
-				<li class="active">
-				    <?php renderConsumer($consumerId, $consumerInfo); ?>
-				</li>
-			<?php endif;?>
-		   </ul>
-		   <?php endif; ?>
+        <td>
+           <?php if ($hasActiveConsumers) : ?>
+           <h3 class="active" >Active consumer groups</h3>
+           <ul>
+            <?php foreach($consumers as $consumerId => $consumerInfo) if ($consumerInfo['active']) : ?>
+                <li class="active">
+                    <?php renderConsumer($consumerId, $consumerInfo); ?>
+                </li>
+            <?php endif;?>
+           </ul>
+           <?php endif; ?>
 
            <?php if ($hasInactiveConsumers) : ?>
-		   <h3 class="inactive" >Inactive consumer groups</h3>
+           <h3 class="inactive" >Inactive consumer groups</h3>
            <ul>
             <?php foreach($consumers as $consumerId => $consumerInfo) if (!$consumerInfo['active'] && !$consumerInfo['abandoned']) : ?>
                 <li class="inactive">
@@ -211,20 +221,20 @@ try {
         <td>
             <h3 class="abandoned">Abandoned consumer groups</h3>
             <ul>
-	        <?php foreach($consumers as $consumerId => $consumerInfo) if ($consumerInfo['abandoned']) : ?>
-	        <li class="abandoned">
-	        	'<b><?php echo $consumerId;?></b>' had consumed:
-	        	<?php if (isset($consumerInfo['topics'])) foreach($consumerInfo['topics'] as $topicName => $consumerTopic): ?>
-	        	  <small><?php echo $topicName;?></small>, 
-	        	<?php endforeach; ?>
-	        </li>
-	        <?php endif;?>
-	        </ul>
-	    </td>
-	    <?php endif; ?>
-	    <?php endif;?>
-	    </tr>
-	</table>
+            <?php foreach($consumers as $consumerId => $consumerInfo) if ($consumerInfo['abandoned']) : ?>
+            <li class="abandoned">
+                '<b><?php echo $consumerId;?></b>' had consumed:
+                <?php if (isset($consumerInfo['topics'])) foreach($consumerInfo['topics'] as $topicName => $consumerTopic): ?>
+                  <small><?php echo $topicName;?></small>, 
+                <?php endforeach; ?>
+            </li>
+            <?php endif;?>
+            </ul>
+        </td>
+        <?php endif; ?>
+        <?php endif;?>
+        </tr>
+    </table>
 </body>
 
 <?php function renderConsumer($consumerId, array $consumerInfo) { ?>
